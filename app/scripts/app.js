2b1cd94b5817e4d52278be3e084e9cb4d05d29bf
pradeep <pradeep.b@harnesstouch.com>
2015-11-08 18:36:12 +0530

    the first version of the running app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularprogressbarApp
+ * @description
+ * # angularprogressbarApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularprogressbarApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularprogressbarApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularprogressbarApp
+ */
+angular.module('angularprogressbarApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
